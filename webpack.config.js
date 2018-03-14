fd7cf0798401e471d76e0bcb6b3ebb8b4a9d08dc
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2018-03-14 13:10:28 -0700

    UPDATE webpack.config.js and REAMDE

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 module.exports = {
   // Add the .caf extension to the extensions webpack looks for...
-  resolve: { extensions: [".caf", ".js"] },
+  resolve:  { extensions: [".caf", ".js"] },
 
   // Add the caffeine-script webpack-loader so webpack knows how to load caf files...
-  module: {rules: [{ test: /\.caf?$/, loader: "caffeine-mc/webpack-loader" }]},
-
-  // standard webpack boilerplate
-  mode: 'production',   // change to 'development' to turn off uglify
-  output: { filename: "[name].js" },
+  module:   { rules: [{ test: /\.caf$/, loader: "caffeine-mc/webpack-loader" }]},
 
   // configured to compile our ./hello file.
   // Note, this looks for the FIRST MATCH from the extensions list above:
   //   Example: hello.caf matches before hello.js
-  entry: { hello: "./hello" }
+  entry:    { hello: "./hello" },
+
+  // standard webpack boilerplate:
+  //  make the output file name the same as the 'entry' name (but with .js)
+  output:   { filename: "[name].js" },
+
+  // change mode to 'production' to generate minimized code
+  mode:     'development'
 };
